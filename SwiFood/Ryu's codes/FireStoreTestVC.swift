4c2b1e92f00d3a212feee79eea8857f0d09bffa5
Chang-Geun-Ryu <goodrcg@gmail.com>
2019-05-25 19:21:07 +0900

    정리

---
+++ SwiFood/Ryu's codes/FireStoreTestVC.swift
@@ -0,0 +1,17 @@
+//
+//  FireStoreTestVC.swift
+//  SwiFood
+//
+//  Created by CHANGGUEN YU on 25/05/2019.
+//  Copyright © 2019 Swifood Team. All rights reserved.
+//
+
+import UIKit
+
+class FireStoreTestVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
