6f59d6fb884b3b75ce532ff057a1ce6a271d9dc6
92pino <jjb8382@gmail.com>
2019-06-19 18:25:15 +0900

    signup firebase storage & 실시간데이터베이스 upload finish

--- SwiFood/MyPage/SignUpViewController.swift
+++ SwiFood/MyPage/SignUpViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 import FirebaseAuth
 
 class SignUpViewController: UIViewController {
+    
+    var imageSelected = false
+    
+    var profileImage = UIImage(named: "")
+    var profileImageName = ""
+    let profileimageView = UIImageView(image: UIImage(named: "img"))
+    
+    var ref: DatabaseReference!
     
     let signUpTitle: UILabel = {
         let label = UILabel()
@@ -26,44 +36,77 @@ class SignUpViewController: UIViewController {
         return button
     }()
     
-    let signUpFieldView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        
-        return view
-    }()
-    
-    let profileImageView: UIImageView = {
-        let imageView = UIImageView()
-        
-        return imageView
+    let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
     }()
     
     let nickName: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "닉네임"
-        textField.leftViewMode = UITextField.ViewMode.always
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "닉네임"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.leftViewMode = UITextField.ViewMode.always
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
         let image = #imageLiteral(resourceName: "user")
         imageView.image = image
-        textField.leftView = imageView
+        tf.leftView = imageView
         
-        return textField
+        return tf
     }()
     
-    let userEmail: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "이메일"
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.leftViewMode = UITextField.ViewMode.always
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let image = #imageLiteral(resourceName: "email")
+        imageView.image = image
+        tf.leftView = imageView
+        tf.addTarget(self, action: #selector(formValidation), for: .editingChanged)
         
-        return textField
+        return tf
     }()
     
-    let userPassword: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "비밀번호"
-        textField.isSecureTextEntry = true
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.isSecureTextEntry = true
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.leftViewMode = UITextField.ViewMode.always
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let image = #imageLiteral(resourceName: "icons8-lock-filled-50")
+        imageView.image = image
+        tf.leftView = imageView
+        tf.addTarget(self, action: #selector(formValidation), for: .editingChanged)
         
-        return textField
+        return tf
+    }()
+    
+    let confimPasswordUserTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.isSecureTextEntry = true
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.leftViewMode = UITextField.ViewMode.always
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let image = #imageLiteral(resourceName: "icons8-lock-filled-50")
+        imageView.image = image
+        tf.leftView = imageView
+        tf.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        
+        return tf
     }()
     
     let completeSignUpButton: UIButton = {
@@ -72,6 +115,7 @@ class SignUpViewController: UIViewController {
         button.setTitle("회원가입", for: .normal)
         button.backgroundColor = #colorLiteral(red: 0.6117647059, green: 0.2, blue: 0.2588235294, alpha: 1)
         button.tintColor = .white
+        button.isEnabled = false
         
         return button
     }()
@@ -80,8 +124,6 @@ class SignUpViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        dismissButton.addTarget(self, action: #selector(dismissButton(_:)), for: .touchUpInside)
-        
         configure()
     }
     
@@ -89,19 +131,16 @@ class SignUpViewController: UIViewController {
         
         view.addSubview(dismissButton)
         view.addSubview(signUpTitle)
-        view.addSubview(signUpFieldView)
-        signUpFieldView.addSubview(profileImageView)
-        signUpFieldView.addSubview(nickName)
-        signUpFieldView.addSubview(userPassword)
+        view.addSubview(plusPhotoButton)
         view.addSubview(completeSignUpButton)
         
+        completeSignUpButton.isEnabled = false
+        dismissButton.addTarget(self, action: #selector(dismissButton(_:)), for: .touchUpInside)
+        plusPhotoButton.addTarget(self, action: #selector(handleSelectProfilePhoto(_:)), for: .touchUpInside)
+        
         autoLayout()
-        
+        configureViewComponents()
         completeSignUpEvent()
-    }
-    
-    private func signUpField() {
-        
     }
     
     private func completeSignUpEvent() {
@@ -112,33 +151,146 @@ class SignUpViewController: UIViewController {
         let guide = view.safeAreaLayoutGuide
         dismissButton.layout.top(equalTo: signUpTitle.topAnchor).trailing(equalTo: view.trailingAnchor, constant: -30)
         signUpTitle.layout.top(equalTo: guide.topAnchor, constant: 10).centerX()
-        signUpFieldView.top(equalTo: signUpTitle.bottomAnchor, constant: 10)
-        nickName.layout.centerX().centerY(equalTo: guide.centerYAnchor, constant: -200)
-        completeSignUpButton.layout.bottom(equalTo: guide.bottomAnchor, constant: -10).leading(equalTo: guide.leadingAnchor, constant: 30).trailing(equalTo: guide.trailingAnchor, constant: -30)
         
-        signUpFieldView.topAnchor.constraint(equalTo: signUpTitle.bottomAnchor, constant: 10).isActive = true
-        signUpFieldView.bottomAnchor.constraint(equalTo: completeSignUpButton.topAnchor, constant: 10).isActive = true
-        signUpFieldView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
-        signUpFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusPhotoButton.topAnchor.constraint(equalTo: signUpTitle.bottomAnchor, constant: 50).isActive = true
+        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
         
+        completeSignUpButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40).isActive = true
+        completeSignUpButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        completeSignUpButton.topAnchor.constraint(equalTo: view.bottomAnchor, constant: -80).isActive = true
         completeSignUpButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    
+    func configureViewComponents() {
+        
+        let stackView = UIStackView(arrangedSubviews: [nickName, emailTextField, passwordTextField, confimPasswordUserTextField])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.distribution = .fillEqually
+        
+        view.addSubview(stackView)
+        
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40).isActive = true
+        
     }
     
     @objc private func dismissButton(_ sender: UIButton) {
         dismiss(animated: true)
     }
     
+    @objc func handleSelectProfilePhoto(_ sender: UIButton) {
+        // configure Image Picker
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        
+        // present image picker
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @objc func formValidation() {
+        
+        guard
+            nickName.hasText,
+            emailTextField.hasText,
+            passwordTextField.hasText,
+            confimPasswordUserTextField.hasText,
+            passwordTextField.text == confimPasswordUserTextField.text,
+            imageSelected == true else {
+                completeSignUpButton.isEnabled = false
+                
+                return
+        }
+        
+        completeSignUpButton.isEnabled = true
+        
+    }
+    
     @objc private func completeSignUp(_ sender: UIButton) {
-        Auth.auth().createUser(withEmail: userEmail.text!, password: userPassword.text!) { (user, error) in
+        
+        // properties
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            
+            // handle error
             if let error = error {
                 print("Failed to create user with error: ", error.localizedDescription)
                 return
             }
             
+            // set profile image
+            guard let profileImg = self.plusPhotoButton.imageView?.image else { return }
+            
+            // upload data
+            guard let uploadData = profileImg.jpegData(compressionQuality: 0.3) else { return }
+            
+            
+            // place image in firebase storage
+           
+            self.ref = Database.database().reference()
+            let keyPath = self.ref.child("Profile").childByAutoId()
+           
+            keyPath.updateChildValues(["nickName":self.nickName.text!])
+            keyPath.updateChildValues(["profileImage":self.profileImageName])
+            Storage.storage().reference().child("profile_images").child(self.profileImageName).putData(uploadData, metadata: nil, completion: { (metadata, error) in
+                
+                // handle error
+                if let error = error {
+                    print("failed to upload image to Firebase Storage with error.", error.localizedDescription)
+                }
+                
+            })
+            
             // success
             print("Successfully created with Firebase")
+            
+            
+            
         }
-        dismiss(animated: true)
+        let tabbar = UIApplication.shared.keyWindow?.rootViewController as? UITabBarController
+        tabbar?.selectedIndex = 0
+        self.dismiss(animated: true)
     }
 
+}
+
+extension SignUpViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // selected image
+        guard let profile = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            
+            imageSelected = false
+            return
+            
+        }
+        
+        guard let editData = profile.jpegData(compressionQuality: 0.3) else { return print("editImage")}
+        guard let editImage = UIImage(data: editData) else { return }
+        let imageName = "\(Int(NSDate.timeIntervalSinceReferenceDate * 1000))"
+        
+        profileImageName = imageName
+        profileImage = editImage
+        profileimageView.image = editImage
+        
+        // set imageSelected to true
+        imageSelected = true
+        
+        // configure plusPhotoBtn with selected image
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 2
+        plusPhotoButton.setImage(profile.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
 }
